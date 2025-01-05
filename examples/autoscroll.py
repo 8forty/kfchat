7ed85e9b2c5f66f9e3863d42cbd601dfc517a572
Keith Freeman <8forty@gmail.com>
2025-01-05 15:09:40 -0800

    finally got autoscroll working in autoscroll example

--- examples/autoscroll.py
+++ examples/autoscroll.py
@@ -1,29 +1,38 @@
+import builtins
+
 from nicegui import ui
-from nicegui.elements.scroll_area import ScrollArea
 
 numbers = list(range(0, 10))
 
 
 def setup(path: str):
     @ui.refreshable
-    def show_numbers() -> None:
+    async def show_numbers(scroller_class: str) -> None:
         for i in numbers:
             ui.label(f'scrolling:{i} ')
 
-    def add(scroller: ScrollArea):
+        # make sure client is connected before doing javascript
+        try:
+            await ui.context.client.connected(timeout=5.0)
+            await ui.run_javascript(code=f"const sdiv = document.getElementsByClassName('{scroller_class}')[0].firstElementChild;"
+                                         f"sdiv.scrollTop = sdiv.scrollHeight;")
+            # scroller.scroll_to(percent=1.0)  # this is unpredictable, usually scrolls to penultimate item
+        except builtins.TimeoutError:
+            pass
+
+    def add():
         numbers.append(len(numbers))
         show_numbers.refresh()
-        scroller.scroll_to(percent=100.0)  # this is unpredictable with 1.0, seems better with 100.0?
 
     @ui.page(path)
-    def index():
-        ui.button(text='add', on_click=lambda: add(scroller))
+    async def index():
+        ui.button(text='add', on_click=lambda: add())
 
         with ui.row():
             with ui.card().classes('w-32 h-48'):
-                with ui.scroll_area() as scroller:
-                    show_numbers()
-                    scroller.scroll_to(percent=100.0)  # this works on initial display with 1.0 or 100.0?
+                sclass = 'kfname-scroll-area-scroller1'
+                with ui.scroll_area().classes(add=sclass):
+                    await show_numbers(sclass)
 
 
 if __name__ in {'__main__', '__mp_main__'}:
