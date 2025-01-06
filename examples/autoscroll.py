dd1ba1bd593cec9bf6f706acf1545ab43920a0ba
Keith Freeman <8forty@gmail.com>
2025-01-05 18:52:55 -0800

    autoscroll now sync, async, or manual

--- examples/autoscroll.py
+++ examples/autoscroll.py
@@ -1,15 +1,63 @@
 import builtins
+from typing import Callable, Self, Iterator
 
 from nicegui import ui
+from nicegui.element import Element
+from nicegui.elements.row import Row
+from nicegui.elements.scroll_area import ScrollArea
 
 numbers = list(range(0, 10))
 
 
-def setup(path: str):
+class AutoScrollArea(ScrollArea):
+    def __init__(self, container: Row, elements_function: Callable[[], list[Element]]):
+        super().__init__()
+
+    def __enter__(self) -> Self:
+        self.default_slot.__enter__()
+        return self
+
+    def __exit__(self, *_) -> None:
+        self.default_slot.__exit__(*_)
+
+    def __iter__(self) -> Iterator[Element]:
+        for slot in self.slots.values():
+            yield from slot
+
+    # def update(self):
+    #     self.container.clear()
+    #     for element in self.elfunc():
+    #         self.container.add(element)
+
+
+def setup_manual(path: str):
+    def show_numbers() -> None:
+        for i in numbers:
+            ui.label(f'mscrolling:{i} ')
+
+    def add(scroller: ScrollArea) -> None:
+        numbers.append(len(numbers))
+        scroller.clear()
+        with scroller:
+            show_numbers()
+        scroller.scroll_to(percent=1.1)
+
+    @ui.page(path)
+    def index():
+        ui.button(text='add', on_click=lambda: add(scroller))
+
+        with ui.row():
+            with ui.card().classes('w-40 h-48'):
+                with ui.scroll_area().classes() as scroller:
+                    show_numbers()
+                    scroller.scroll_to(percent=1.0)
+
+
+def setup_refreshable_async(path: str):
     @ui.refreshable
     async def show_numbers(scroller_class: str) -> None:
         for i in numbers:
-            ui.label(f'scrolling:{i} ')
+            ui.label(f'rascrolling:{i} ')
 
         # make sure client is connected before doing javascript
         try:
@@ -29,12 +77,44 @@ def setup(path: str):
         ui.button(text='add', on_click=lambda: add())
 
         with ui.row():
-            with ui.card().classes('w-32 h-48'):
+            with ui.card().classes('w-40 h-48'):
                 sclass = 'kfname-scroll-area-scroller1'
                 with ui.scroll_area().classes(add=sclass):
                     await show_numbers(sclass)
 
 
+def setup_refreshable_sync(path: str):
+    @ui.refreshable
+    def show_numbers(scroller_class: str) -> None:
+        for i in numbers:
+            ui.label(f'rsscrolling:{i} ')
+
+        # make sure client is connected before doing javascript
+        try:
+            # ui.context.client.connected(timeout=5.0)
+            ui.run_javascript(code=f"const sdiv = document.getElementsByClassName('{scroller_class}')[0].firstElementChild;"
+                                   f"sdiv.scrollTop = sdiv.scrollHeight;")
+            # scroller.scroll_to(percent=1.0)  # this is unpredictable, usually scrolls to penultimate item
+        except builtins.TimeoutError:
+            pass
+
+    def add():
+        numbers.append(len(numbers))
+        show_numbers.refresh()
+
+    @ui.page(path)
+    def index():
+        ui.button(text='add', on_click=lambda: add())
+
+        with ui.row():
+            with ui.card().classes('w-40 h-48'):
+                sclass = 'kfname-scroll-area-scroller1'
+                with ui.scroll_area().classes(add=sclass):
+                    show_numbers(sclass)
+
+
 if __name__ in {'__main__', '__mp_main__'}:
-    setup('/')
+    # setup_refreshable_sync('/')
+    # setup_refreshable_async('/')
+    setup_manual('/')
     ui.run(host='localhost', port=8000, show=False, reload=False)
