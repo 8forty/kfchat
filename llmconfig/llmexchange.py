d46830eba167fd317793eae6aafb3b24f07aa9da
Keith Freeman <8forty@gmail.com>
2025-02-22 13:11:14 -0800

    small llmexchange update

--- llmconfig/llmexchange.py
+++ llmconfig/llmexchange.py
@@ -2,6 +2,7 @@ import logging
 from abc import ABC
 
 import logstuff
+from llmconfig.llmconfig import LLMSettings
 
 log: logging.Logger = logging.getLogger(__name__)
 log.setLevel(logstuff.logging_level)
@@ -12,11 +13,21 @@ class LLMResponse(ABC):
         self.role = role
         self.content = content
 
+    def __repr__(self) -> str:
+        return f'[{self.__class__!s}:{self.__dict__!r}]'
+
 
 class LLMExchange(ABC):
-    def __init__(self, prompt: str, responses: list[LLMResponse], response_duration_seconds: float, problems: dict[int, str]):
+    def __init__(self, prompt: str, provider: str, model_name: str, settings: LLMSettings, responses: list[LLMResponse],
+                 response_duration_seconds: float, problems: dict[int, str]):
         self.prompt = prompt
+        self.provider = provider
+        self.model_name = model_name
+        self.settings = settings
         self.responses = responses
         self.response_duration_secs = response_duration_seconds
         self.problems = problems  # [response-idx, problem-description]
+
+    def __repr__(self) -> str:
+        return f'[{self.__class__!s}:{self.__dict__!r}]'
 
--- llmconfig/llmoaiexchange.py
+++ llmconfig/llmoaiexchange.py
@@ -3,6 +3,7 @@ import logging
 from openai.types.chat import ChatCompletion
 
 import logstuff
+from llmconfig.llmconfig import LLMSettings
 from llmconfig.llmexchange import LLMExchange, LLMResponse
 
 log: logging.Logger = logging.getLogger(__name__)
@@ -10,9 +11,16 @@ log.setLevel(logstuff.logging_level)
 
 
 class LLMOaiExchange(LLMExchange):
-    def __init__(self, prompt: str, chat_completion: ChatCompletion, response_duration_seconds: float):
+    def __init__(self, prompt: str, chat_completion: ChatCompletion,
+                 provider: str,
+                 model_name: str,
+                 settings: LLMSettings,
+                 response_duration_seconds: float):
         stop_problem_reasons = ['length', 'too many tokens', 'content_filter', 'flagged by content filter', 'tool_calls', 'called a tool', 'function_call']
         super().__init__(prompt=prompt,
+                         provider=provider,
+                         model_name=model_name,
+                         settings=settings,
                          responses=[LLMResponse(choice.message.role, choice.message.content) for choice in chat_completion.choices],
                          response_duration_seconds=response_duration_seconds,
                          problems={idx: choice.finish_reason for idx, choice in enumerate(chat_completion.choices) if choice.finish_reason in stop_problem_reasons})
